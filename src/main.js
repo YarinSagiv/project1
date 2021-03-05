55d210a2d24cf378e5174e920f66b239044aa2e1
Mor Gajer <morgajer2@gmail.com>
2021-03-05 18:02:31 +0200

    added npm

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const v = 2;
+console.log(v);
+var arr = ['r','e'];
+console.log(arr);
---
+++ test/array.spec.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+describe(title = 'Array', fn = () => {
+    describe(title = '#sort', fn = () => {
+        it(title = 'should sorting array by name', fn = () => {
+            var names = ['Mor', 'Yarin', 'Noa', 'Ofir'];
+            expect(names.sort()).to.be.eql(value = ['Mor', 'Noa', 'Ofir', 'Yarin']);
+        })
+
+    })
+})
+
+
