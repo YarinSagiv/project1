99de4235e9f202a73de652d51df759a30bb7573a
Mor Gajer <morgajer2@gmail.com>
2021-03-11 14:01:19 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app_port = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => { res.sendfile("firstpage.html");});
+app.get("/", (req, res) => { res.sendfile("src/firstpage.html");});
 
 app.listen(app_port);
 console.log(`app is running. port: ${app_port}`);
