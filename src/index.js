3db5ad6d613c76f44abeb6b875c0eb9272f186b2
Mor Gajer <morgajer2@gmail.com>
2021-03-13 19:30:44 +0200

    fixed css

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const app_port = process.env.PORT || 3000;
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
+// Render static files
+app.use(express.static('public'));
 
-//app.get("/", (req, res) => { res.sendfile("src/firstpage.ejs");});
 
 app.listen(app_port);
 console.log(`app is running. port: ${app_port}`);
@@ -21,60 +22,18 @@ app.get('/', function (req, res) {
     res.render('pages/firstpage');
 });
 
-router.get('/logIn',function(req,res){
+app.get('/logIn', function (req, res) {
     res.render('pages/logIn')
-  })
+})
+
+app.get('/connectpage', function (req, res) {
+    res.render('pages/connectpage')
+})
+
+app.get('/newu', function (req, res) {
+    res.render('pages/newu')
+})
+
 
 // https://project1sprint1.herokuapp.com/
 
-
-
-const express = require('express')
-const app_port = process.env.PORT|| 3000
-const app = express()
-const router = express.Router()
-const bodyParser= require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-const connectionString = 'mongodb+srv://team15:Ade123321!@cluster0.3jopa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-
-MongoClient.connect(connectionString, { 
-  useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('MedicalDB')
-    const usersCollection = db.collection('users')
-    app.set('view engine', 'ejs')
-
-    app.use(express.static('public'))
-
-    app.use(bodyParser.urlencoded({ extended: true }))
-
-    router.get('/',function(req,res){
-      res.status(200).render('home')
-    })
-
-    router.get('/login',function(req,res){
-      res.status(200).render('login')
-    })
-
-    router.get('/signup',function(req,res){
-      res.status(200).render('signup')
-    })
-    
-    router.post('/signup', (req, res) => {
-      usersCollection.insertOne(req.body)
-        .then(
-          res.redirect('/')
-        )
-        .catch(error => console.error(error))
-    })
-    app.use('/', router)//add the router
-    
-    
-  })
-  .catch(error => console.error(error))
-
-module.exports = app.listen(app_port)
-console.log(`app is running. port: ${app_port}`)
-console.log(`http://127.0.0.1:${app_port}/`)
