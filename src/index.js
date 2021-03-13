8501ebbf699c9cd5ee6838af3fe2963f4949d295
Mor Gajer <morgajer2@gmail.com>
2021-03-13 19:37:01 +0200

    updated lint and index and fixed errors

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const app = express();
 const app_port = process.env.PORT || 3000;
 
 // Set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // Render static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 
 app.listen(app_port);
@@ -18,21 +18,21 @@ console.log(`http://127.0.0.1:${app_port}/`);
 
 // *** GET Routes - display pages ***
 // Root Route
-app.get('/', function (req, res) {
-    res.render('pages/firstpage');
+app.get("/", function (req, res) {
+    res.render("pages/firstpage");
 });
 
-app.get('/logIn', function (req, res) {
-    res.render('pages/logIn')
-})
+app.get("/logIn", function (req, res) {
+    res.render("pages/logIn");
+});
 
-app.get('/connectpage', function (req, res) {
-    res.render('pages/connectpage')
-})
+app.get("/connectpage", function (req, res) {
+    res.render('pages/connectpage');
+});
 
 app.get('/newu', function (req, res) {
-    res.render('pages/newu')
-})
+    res.render('pages/newu');
+});
 
 
 // https://project1sprint1.herokuapp.com/
