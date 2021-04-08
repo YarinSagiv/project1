178533e0ebcba02f00e838b93e9bb081fee96cf4
Mor Gajer <morgajer2@gmail.com>
2021-04-08 13:30:38 +0300

    updated connection,js

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,6 @@
-const {MongoClient} = require('mongodb');
+//const {MongoClient} = require('mongodb');
 
-
+/*
 async function listDatabases(client){
     var databasesList = await client.db().admin().listDatabases();
  
@@ -12,6 +12,7 @@ async function listDatabases(client){
 async function main() {
 
     const uri = "mongodb+srv://ymon:ymonashdod@cluster.0qqlp.mongodb.net/eventSaver?retryWrites=true&w=majority";
+
     const client = new MongoClient(uri);
     await client.connect();
     await listDatabases(client);
@@ -30,4 +31,90 @@ async function main() {
 }
 
 main().catch(console.error);
+*/
+//MUST!
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://ymon:ymonashdod@cluster.0qqlp.mongodb.net/eventSaver?retryWrites=true&w=majority";
 
+/*// -- create db
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
+// -- create collenction:
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("eventSaver");
+  dbo.createCollection("Employers", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});*/
+
+/*// -- insert example:
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("eventSaver");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbo.collection("Employers").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+
+  //-- delete document example:
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("eventSaver");
+    var myquery = { address: 'Highway 37' };
+    dbo.collection("Employers").deleteOne(myquery, function (err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        db.close();
+    });
+});
+
+
+//-- update document example:
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("eventSaver");
+    var myquery = { address: "Valley 345" };
+    var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+    dbo.collection("Employers").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      db.close();
+    });
+  });
+
+
+
+//-- find all documents example:
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("eventSaver");
+    dbo.collection("Employers").findOne({}, function(err, result) { //find ALL 
+      if (err) throw err;
+      console.log(result.name);
+      db.close();
+    });
+  });
+
+// -- find documents by a query example:
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("eventSaver");
+    var query = { address: "Park Lane 38" };
+    dbo.collection("Employers").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+
+  */
