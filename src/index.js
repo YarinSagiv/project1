c446c17849f96eaec9cd3aa1f9846dbd6f701b52
Mor Gajer <morgajer2@gmail.com>
2021-04-11 00:08:59 +0300

    updated updateProfileContractor - now working with GET and POST, and has communication with MongoDB

--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ const app = express();
 
 const app_port = process.env.PORT || 4000;
 
+const MongoClient = require('mongodb').MongoClient;
+const { request } = require("express");
+const url = "mongodb+srv://ymon:ymonashdod@cluster.0qqlp.mongodb.net/eventSaver?retryWrites=true&w=majority";
+
+
+
 // Set the view engine to ejs
 app.set("view engine", "ejs");
 // Render static files
 app.use(express.static("public"));
+app.use(express.urlencoded({
+    extended: true
+}))
 
 
 app.listen(app_port);
@@ -35,8 +44,53 @@ app.get('/newu', function (req, res) {
 });
 
 app.get('/updateProfileContractor', function (req, res) {
-    res.render('pages/updateProfileContractor');
+    /* 
+        get ditails from db
+    */
+    var Uid = 1234; //the user's ID after logging in //TODO
+    var info = "";
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("eventSaver");
+        //var query = { _id: Uid };
+        var query = { firstName: "mor" };
+        dbo.collection("Employers").find(query).toArray(function (err, result) {
+            if (err) throw err;
+            if (result.length != 0) {
+                console.log(result[0].lastName); //test
+                //info = result[0];
+                res.render('pages/updateProfileContractor', result[0]);
+            }
+            db.close();
+        });
+    });
+
+    /*
+      redirect to the update page and send the ditails
+    */
+    console.log("info " + info);
+    //res.render('pages/updateProfileContractor', { name: "mor" });
 });
+
+app.post('/updateContractor', (req, res) => {
+    console.log(req.body.phone);
+    //------TEST:
+    /*
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("eventSaver");
+        var myobj = { firstName: req.body.firstname, lastName: req.body.lastname };
+        dbo.collection("Employers").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      });*/
+
+    res.end();
+})
+
 
 app.get('/updateProfileEmployer', function (req, res) {
     res.render('pages/updateProfileEmployer');
