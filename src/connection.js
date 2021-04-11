edc2aef1c3d4ea9e578ffdbcb3c9058fb3366b0c
Mor Gajer <morgajer2@gmail.com>
2021-04-11 20:53:00 +0300

    updated index and updateContractor and updateEmployer

--- src/connection.js
+++ src/connection.js
@@ -54,14 +54,16 @@ MongoClient.connect(url, function(err, db) {
   });
 });*/
 
-/*// -- insert example:
+// -- insert example:
+/*
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("eventSaver");
-    var myobj = { name: "Company Inc", address: "Highway 37" };
-    dbo.collection("Employers").insertOne(myobj, function(err, res) {
+    var arr = [1,2,3,4];
+    var myobj = { name: "Company Inc", address: arr };
+    dbo.collection("ContractorWorkers").insertOne(myobj, function(err, res) {
       if (err) throw err;
-      console.log("1 document inserted");
+      console.log("1 M document inserted");
       db.close();
     });
   });
@@ -118,3 +120,29 @@ MongoClient.connect(url, function(err, db) {
   });
 
   */
+
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("eventSaver");
+    var query = { _id: "208394700" };
+    dbo.collection("ContractorWorkers").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+/*
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("eventSaver");
+    var myquery = { _id: "208394700" };
+    var newvalues = { $set: {firstName: "Mickey", lastName: "Canyon 123" } };
+    dbo.collection("ContractorWorkers").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      db.close();
+    });
+  });
+
+*/
