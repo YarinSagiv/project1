0a42f699d6dc82d7f85a90ea7b287ebeb7906427
Mor Gajer <morgajer2@gmail.com>
2021-04-16 11:44:58 +0300

    final changes in the update profile

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app_port = process.env.PORT || 4000;
 const MongoClient = require('mongodb').MongoClient;
 const { request } = require("express");
 const url = "mongodb+srv://ymon:ymonashdod@cluster.0qqlp.mongodb.net/eventSaver?retryWrites=true&w=majority";
-var Uid = "208394701";
+var Uid = "208394700";
 
 
 // Set the view engine to ejs
@@ -40,7 +40,7 @@ app.get("/connectpage", function (req, res) {
 });
 
 app.get('/newu', function (req, res) {
-    res.render('pages/newu',{suc1: true});
+    res.render('pages/newu', { suc1: true });
 });
 
 app.get('/updateProfileContractor', function (req, res) {
@@ -48,7 +48,9 @@ app.get('/updateProfileContractor', function (req, res) {
         get ditails from db
     */
     //the user's ID after logging in //TODO
-    var info = "";
+
+    if (Uid == "")
+        res.redirect("/");
 
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
@@ -69,7 +71,6 @@ app.get('/updateProfileContractor', function (req, res) {
     /*
       redirect to the update page and send the ditails
     */
-    console.log("info " + info);
     //res.render('pages/updateProfileContractor', { name: "mor" });
 });
 
@@ -82,20 +83,20 @@ app.post('/updateContractor', (req, res) => {
         var myquery = { _id: Uid };
 
         var hasA, theA, area; //address
-        if (typeof req.body.addressC != "undefined"){
+        if (typeof req.body.addressC != "undefined") {
             hasA = true;
             console.log("hasA 1");
         }
         else
             hasA = false;
-        if(hasA)
-            theA=req.body.address;
+        if (hasA)
+            theA = req.body.address;
         else
-            theA=null;
-        if(req.body.areas=="")
-            area=null;
+            theA = null;
+        if (req.body.areas == "")
+            area = null;
 
-        var newvalues = { $set: { firstName: req.body.firstname, lastName: req.body.lastname, email: req.body.email, hasAddress: hasA, address: theA, phoneNumbers:req.body.phone, countryAreas:area, jobTypes:req.body.types } };
+        var newvalues = { $set: { firstName: req.body.firstname, lastName: req.body.lastname, email: req.body.email, hasAddress: hasA, address: theA, phoneNumbers: req.body.phone, countryAreas: area, jobTypes: req.body.types } };
 
         dbo.collection("ContractorWorkers").updateOne(myquery, newvalues, function (err, res) {
             if (err) throw err;
@@ -112,61 +113,64 @@ app.post('/updateContractor', (req, res) => {
 app.post('/inputDataBase', (req, res) => {
 
 
-      MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("eventSaver");
-        var myobj = { _id: req.body.iduser,
-                      firstName:  req.body.firstname,
-                      lastName: req.body.secname,
-                      phoneNumbers: req.body.phone,
-                      userName: req.body.username,
-                      email: req.body.email,
-                      password:req.body.psw };
-       
-        
-        var succ=dbo.collection("Employers").insertOne(myobj, function(err, res1) {
+        var myobj = {
+            _id: req.body.iduser,
+            firstName: req.body.firstname,
+            lastName: req.body.secname,
+            phoneNumbers: req.body.phone,
+            userName: req.body.username,
+            email: req.body.email,
+            password: req.body.psw
+        };
 
-          if (err) 
-          {
-            
-            res.render("pages/newu",{suc1 : "false"}); 
-            console.log("ilanit");
-          }
-          else
-          {
-            res.render("pages/firstpage",{suc1 : "true"}); //the response 
-          }
 
-        
-          console.log("1 document inserted");
-          db.close();
-         
-          });
+        var succ = dbo.collection("Employers").insertOne(myobj, function (err, res1) {
+
+            if (err) {
+
+                res.render("pages/newu", { suc1: "false" });
+                console.log("ilanit");
+            }
+            else {
+                res.render("pages/firstpage", { suc1: "true" }); //the response 
+            }
+
+
+            console.log("1 document inserted");
+            db.close();
 
         });
-        
-    
 
-        
-        
-      
-    
+    });
+
+
+
+
+
+
+
 });
 
 
-app.post('/loginInCheck',(req, res) => {
-    
-    MongoClient.connect(url, function(err, db) {
+app.post('/loginInCheck', (req, res) => {
+
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("eventSaver");
-    
-    
-});
+
+
+    });
 });
 
 
 
 app.get('/updateProfileEmployer', function (req, res) {
+
+    if (Uid == "")
+        res.redirect("/");
 
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
@@ -189,9 +193,22 @@ app.get('/updateProfileEmployer', function (req, res) {
 
 
 app.post('/updateProfileEmployer', function (req, res) {
-    //res.render('pages/updateProfileEmployer');
-    console.log(req.body.firstName);
-    res.end();
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("eventSaver");
+        var myquery = { _id: Uid };
+
+        var newvalues = { $set: { firstName: req.body.firstname, lastName: req.body.lastname, email: req.body.email, phoneNumbers: req.body.phone } };
+
+        dbo.collection("Employers").updateOne(myquery, newvalues, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+            db.close();
+        });
+    });
+
+    //res.send({redirect: '/blog'});
+    res.redirect('/');
 });
 
 // https://project1sprint1.herokuapp.com
